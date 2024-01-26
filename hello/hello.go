0ebf5d7a17e12e2bb47f9bbcdbb35a1457bcd4d6
greenfructose <info@justinturney.com>
2024-01-26 11:26:56 -0600

    Formatting

--- hello/hello.go
+++ hello/hello.go
@@ -6,9 +6,9 @@ import (
 )
 
 func Hello() {
-    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
-    })
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
+	})
 
-    http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(":8080", nil)
 }
