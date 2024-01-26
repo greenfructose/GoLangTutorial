a551e1ce078d35a01e51e17f0c4b1c28f03a7d65
greenfructose <info@justinturney.com>
2024-01-26 11:37:41 -0600

    Added router

--- hello/hello.go
+++ hello/hello.go
@@ -3,15 +3,20 @@ package hello
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func Hello() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
-	})
+	r := mux.NewRouter()
 
-	fs := http.FileServer(http.Dir("static/"))
-	http.Handle("/static/", http.StripPrefix("/static/", fs))
+	r.HandleFunc("/books/{title}/page/{page}", func(w http.ResponseWriter, r *http.Request) {
+        vars := mux.Vars(r)
+        title := vars["title"]
+        page := vars["page"]
 
-	http.ListenAndServe(":8080", nil)
+        fmt.Fprintf(w, "You've requested the book: %s on page %s\n", title, page)
+    })
+
+	http.ListenAndServe(":8080", r)
 }
