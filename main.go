986d94fce1af67313b9e6f3cb378f8073717ac55
greenfructose <info@justinturney.com>
2024-01-26 13:31:53 -0600

    change package name

--- main.go
+++ main.go
@@ -1,14 +1,14 @@
 package main
 
 import (
-	"github.com/greenfructose/golangtutorial/album"
 	"github.com/gin-gonic/gin"
+	"github.com/greenfructose/golangtutorial/items"
 )
 
 
 func main() {
 	router := gin.Default()
-	router.GET("/albums", album.getAlbums)
+	router.GET("/albums", items.getAlbums)
 
 	router.Run("localhost:8080")
 }
